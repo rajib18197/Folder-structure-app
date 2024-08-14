296218f1e9ad25a06eea6487ee9c0cc1db81d44c
rajib18197 <rajib18197@gmail.com>
2024-08-14 15:32:30 +0600

    added icons

--- src/Folders.jsx
+++ src/Folders.jsx
@@ -2,21 +2,26 @@ import { useState } from "react";
 import { root } from "./data/tree";
 
 export default function Folders() {
-  const [data, setData] = useState(root);
+  const [data, setData] = useState([root]);
 
   return (
     <div className="folders">
       <h4 className="folders__title">Explorer</h4>
       <header className="folders__header">
         <p>Files-Folders</p>
-        <button>📁 Folder</button>
-        <button>📁 File</button>
+        <button>
+          <ion-icon name="document-outline"></ion-icon>
+          File
+        </button>
+        <button>
+          <ion-icon name="folder-outline"></ion-icon> Folder
+        </button>
       </header>
 
       <main className="folders__main">
-        {/* {data.map((node) => ( */}
-        <Folder node={data} />
-        {/* ))} */}
+        {data.map((node) => (
+          <Folder node={node} key={crypto.randomUUID()} />
+        ))}
       </main>
     </div>
   );
@@ -28,7 +33,15 @@ function Folder({ node }) {
   return (
     <div className="parent">
       <div className="name" onClick={() => setIsOpen((open) => !open)}>
-        {node.childrens.length ? <span>{isOpen ? "🔽" : "▶️"}</span> : null}
+        {node.childrens.length ? (
+          isOpen ? (
+            <ion-icon name="chevron-down-outline"></ion-icon>
+          ) : (
+            <ion-icon name="chevron-forward-outline"></ion-icon>
+          )
+        ) : (
+          <ion-icon name="logo-nodejs"></ion-icon>
+        )}
         <span>{node.name}</span>
       </div>
 
