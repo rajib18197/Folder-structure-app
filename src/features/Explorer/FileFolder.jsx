01af0367e1cd138593b01948f6db562945d065e7
rajib18197 <rajib18197@gmail.com>
2024-08-15 14:28:47 +0600

    implemented right click handlers for creating nested file/folders

--- src/features/Explorer/FileFolder.jsx
+++ src/features/Explorer/FileFolder.jsx
@@ -2,17 +2,61 @@ import { useState } from "react";
 
 export default function FileFolder({ node }) {
   const [isOpen, setIsOpen] = useState(node.childrens.length ? true : false);
+  const [isContextMenuOpen, setIsContextMenuOpen] = useState(false);
 
+  function handleClick(e) {
+    e.preventDefault();
+    e.stopPropagation();
+    console.log(e);
+    setIsContextMenuOpen(true);
+  }
   return (
-    <div className="parent">
+    <div className="parent" onContextMenu={handleClick}>
+      {isContextMenuOpen && (
+        <div className="btns btns--top">
+          <button
+            onClick={() => {
+              setFolderInput((folder) => ({
+                ...folder,
+                showInput: false,
+                folderName: "",
+              }));
+              setFileInput((file) => ({ ...file, showInput: !file.showInput }));
+            }}
+          >
+            <ion-icon name="document-outline"></ion-icon>
+            File+
+          </button>
+
+          <button
+            onClick={() => {
+              setFileInput((file) => ({
+                ...file,
+                showInput: false,
+                fileName: "",
+              }));
+
+              setFolderInput((folder) => ({
+                ...folder,
+                showInput: !folder.showInput,
+              }));
+            }}
+          >
+            <ion-icon name="folder-outline"></ion-icon>
+            Folder+
+          </button>
+        </div>
+      )}
       {node.name.includes(".") ? (
-        <div className="details file-details">
+        <div className="details file-details name">
           <ion-icon name="document-outline"></ion-icon>
           <span>{node.name}</span>
         </div>
       ) : (
         <div
-          className="details folder-details"
+          className={`details folder-details name ${
+            isContextMenuOpen ? "context-active" : ""
+          }`}
           onClick={() => setIsOpen((open) => !open)}
         >
           {isOpen ? (
